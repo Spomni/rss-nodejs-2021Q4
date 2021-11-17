dbfecc50a209a2f8692aaa25603c0f29dca591f1
spomni <mikhail.snitko@gmail.com>
2021-11-17 10:44:09 +0300

    test(draft): describe cli test cases

---
+++ ciphering-cli-tool/test/cli.spec.js
@@ -0,0 +1,13 @@
+describe('cli', function () {
+
+  it.todo('Should parse cli options using cli-parser-config')
+  it.todo('Should validate config option')
+  it.todo('Should call cipher with converted options')
+  it.todo('Should catch argv parsing errors')
+  it.todo('Should print a message of the catched argv parsing errors to stderr')
+  it.todo('Should catch options validation errors')
+  it.todo('Should print a message of the catched options validation errors to stderr')
+  it.todo('Should catch unknown errors')
+  it.todo('Should print a human friendly message if an unknown error is catched')
+  it.todo('Should exit with non-zero code if any error is catched')
+})
