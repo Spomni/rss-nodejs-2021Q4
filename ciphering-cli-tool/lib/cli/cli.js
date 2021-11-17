5350237423984cb3f7aaf1b91e696ea6f93a13b9
spomni <mikhail.snitko@gmail.com>
2021-11-17 10:51:29 +0300

    refactor: move cli code to the separate function

---
+++ ciphering-cli-tool/lib/cli/cli.js
@@ -0,0 +1,43 @@
+const { parser } = require('../argv-parser')
+const { cipher } = require('../cipher')
+
+const {
+  isInDebugMode,
+  isKnownError,
+  extractCipherOptions,
+  validateCipherOptions,
+} = require('./cli-helpers')
+
+const parserConfig = require('./cli-parser-config')
+
+const UNKNOWN_ERR_MSG = 'Unknown error. For more information, run the application with the --debug option.\n'
+
+function cli() {
+  try {
+
+    const cliOptions = parser.config(parserConfig).exec()
+    const cipherOptions = extractCipherOptions(cliOptions)
+
+    validateCipherOptions(cipherOptions)
+
+    cipher(cipherOptions)
+
+  } catch (err) {
+
+    if (isKnownError(err)) {
+      process.stderr.write(err.message + '\n')
+    } else {
+      process.stderr.write(UNKNOWN_ERR_MSG)
+    }
+
+    if (isInDebugMode()) {
+      console.log('\n', err)
+    }
+
+    process.exit(1)
+  }
+}
+
+module.exports = {
+  cli,
+}
