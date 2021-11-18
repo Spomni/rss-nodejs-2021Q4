b78620bc5ea2acbfafd72adac51dac8f068464d5
spomni <mikhail.snitko@gmail.com>
2021-11-19 02:39:11 +0300

    test: test lib/cipher/cipher-helpers.js file

--- ciphering-cli-tool/test/cipher/cipher-helpers.spec.js
+++ ciphering-cli-tool/test/cipher/cipher-helpers.spec.js
@@ -1,19 +1,114 @@
+const path = require(`path`)
+
+const jestExtended = require('jest-extended')
+expect.extend(jestExtended)
+
+const srcPath = path.resolve(__dirname, '../../lib/')
+const testPath = path.resolve(__dirname, '../')
+
+function fromSrc(filename) {
+  return path.join(srcPath, filename)
+}
+
+jest.mock(fromSrc(`cipher/cipher-io`))
+jest.mock(fromSrc('transform'))
+
+const {
+  InputStream,
+  OutputStream,
+} = require(fromSrc('cipher/cipher-io'))
+
+const {
+  CaesarEncoder,
+  CaesarDecoder,
+  Rot8Encoder,
+  Rot8Decoder,
+  AtbashTransformer,
+} = require(fromSrc('transform'))
+
+const {
+  getTransformList,
+  getInputStream,
+  getOutputStream,
+} = require(fromSrc('cipher/cipher-helpers'))
+
 describe('cipher-helpers', () => {
   
   describe('getInputStream():', function () {
-    it.todo('Should return an instance of the InputSteam class')
+
+    it('Should return an instance of the InputSteam class', function () {
+      expect(getInputStream()).toBeInstanceOf(InputStream)
+      expect(getInputStream('some/path')).toBeInstanceOf(InputStream)
+    })
   })
   
   describe('getOutputStream():', function () {
-    it.todo('Should return an instance of the OutputSteam class')
+
+    it('Should return an instance of the OutputSteam class', function () {
+      expect(getOutputStream()).toBeInstanceOf(OutputStream)
+      expect(getOutputStream('some/path')).toBeInstanceOf(OutputStream)
+    })
   })
-  
+
   describe('getTransformList():', function () {
-    it.todo('Should return an array of transform streams')
-    it.todo('Should place an AtbashTransformer instance at the same position where acommand "A" is')
-    it.todo('Should place an CaesarEncoder instance at the same position where a command "C1" is')
-    it.todo('Should place an CaesarDecoder instance at the same position where a command "C0" is')
-    it.todo('Should place an Rot8Encoder instance at the same position where a command "R1" is')
-    it.todo('Should place an Rot8Decoder instance at the same position where a command "R0" is')
+
+    it('Should return an array', function () {
+      expect(getTransformList('A-C0-R0')).toBeArray()
+    })
+    
+    it('Should place an AtbashTransformer instance at the same position where acommand "A" is', function () {
+      const config = 'R1-A-C1-A'
+      const transformList = getTransformList(config)
+      
+      config.split('-').forEach((command, index) => {
+        if (command !== 'A') return
+        
+        expect(transformList[index]).toBeInstanceOf(AtbashTransformer)
+      })
+    })
+
+    it('Should place an CaesarEncoder instance at the same position where a command "C1" is', function () {
+      const config = 'R1-A-C1-A-R0-C1-C0'
+      const transformList = getTransformList(config)
+      
+      config.split('-').forEach((command, index) => {
+        if (command !== 'C1') return
+        
+        expect(transformList[index]).toBeInstanceOf(CaesarEncoder)
+      })
+    })
+
+    it('Should place an CaesarDecoder instance at the same position where a command "C0" is', function () {
+      const config = 'C0-A-C1-A-R0-C1-C0'
+      const transformList = getTransformList(config)
+      
+      config.split('-').forEach((command, index) => {
+        if (command !== 'C0') return
+        
+        expect(transformList[index]).toBeInstanceOf(CaesarDecoder)
+      })
+    })
+
+    it('Should place an Rot8Encoder instance at the same position where a command "R1" is', function () {
+      const config = 'R1-A-C1-R1-R0-C1-C0'
+      const transformList = getTransformList(config)
+      
+      config.split('-').forEach((command, index) => {
+        if (command !== 'R1') return
+        
+        expect(transformList[index]).toBeInstanceOf(Rot8Encoder)
+      })
+    })
+
+    it('Should place an Rot8Decoder instance at the same position where a command "R0" is', function () {
+      const config = 'R1-A-C1-R0-R0-C1-C0'
+      const transformList = getTransformList(config)
+      
+      config.split('-').forEach((command, index) => {
+        if (command !== 'R0') return
+        
+        expect(transformList[index]).toBeInstanceOf(Rot8Decoder)
+      })
+    })
   })
 })
