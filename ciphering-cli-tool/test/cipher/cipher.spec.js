9e313b32108f21c3093d084d647786bf82c51ab9
spomni <mikhail.snitko@gmail.com>
2021-11-18 22:22:52 +0300

    test: test lib/cipher/cipher.js file

--- ciphering-cli-tool/test/cipher/cipher.spec.js
+++ ciphering-cli-tool/test/cipher/cipher.spec.js
@@ -1,9 +1,84 @@
+const { cipher } = require('../../lib/cipher')
+const { timeout } = require('../lib/timeout-promise')
+
+const jestExtended = require('jest-extended')
+expect.extend(jestExtended)
+
+jest.mock('stream/promises')
+jest.mock('../../lib/cipher/cipher-helpers')
+
+const { pipeline } = require('stream/promises')
+
+const {
+  getTransformList,
+  getInputStream,
+  getOutputStream,
+} = require('../../lib/cipher/cipher-helpers')
+
+const inputStream = {}
+const outputStream = {}
+const transformList = [{}, {}, {}]
+
+getInputStream.mockReturnValue(inputStream)
+getOutputStream.mockReturnValue(outputStream)
+getTransformList.mockReturnValue(transformList)
+
+pipeline.mockImplementation(async () => await timeout(20))
+
 describe('cipher', () => {
 
-  it.todo('should return promise')
-  it.todo('should get input stream by input option')
-  it.todo('should get output stream by output option')
-  it.todo('should get transforms by config option')
-  it.todo('should pipe input, output and transform streams using promised pipeline')
-  it.todo('should be resolved later than pipeline')
+  it('should return promise', async function () {
+    expect(cipher()).toBeInstanceOf(Promise)
+  })
+
+  it('should get input stream by input option', function () {
+    const input = {}
+
+    cipher({ input })
+    expect(getInputStream).toHaveBeenLastCalledWith(input)
+
+    cipher()
+    expect(getInputStream).toHaveBeenLastCalledWith(undefined)
+  })
+
+  it('should get output stream by output option', function () {
+    const output = {}
+
+    cipher({ output })
+    expect(getOutputStream).toHaveBeenLastCalledWith(output)
+
+    cipher()
+    expect(getOutputStream).toHaveBeenLastCalledWith(undefined)
+  })
+
+  it('should get transforms by config option', function () {
+    const config = {}
+
+    cipher({ config })
+    expect(getTransformList).toHaveBeenLastCalledWith(config)
+  })
+
+  it('should pipe input, output and transform streams using promised pipeline', function () {
+    cipher()
+
+    expect(pipeline)
+      .toHaveBeenLastCalledWith(
+        inputStream,
+        ...transformList,
+        outputStream
+      )
+  })
+
+  it('should be resolved later than pipeline', async function () {
+    let isPipelineFullfilled = false
+
+    pipeline.mockImplementationOnce(async () => {
+      await timeout(20)
+      isPipelineFullfilled = true
+    })
+
+    await cipher()
+    
+    expect(isPipelineFullfilled).toBe(true)
+  })
 })
