f73b37791183f315a043f15fb232f9d012585a26
spomni <mikhail.snitko@gmail.com>
2021-11-19 14:37:49 +0300

    test(draft): describe cipher-io test cases

---
+++ ciphering-cli-tool/test/cipher/cipher-io.spec.js
@@ -0,0 +1,14 @@
+
+describe('cipher-io', function () {
+
+  describe('InputStream', function () {
+    it.todo('Should return a Readable stream instance')
+    it.todo('Should read data from the passed file')
+    it.todo('Should be ended when an input file is readed')
+  })
+  
+  describe('OutputStream', function () {
+    it.todo('Should return a Writable stream instance')
+    it.todo('Should append data to the passed file')
+  })
+})
