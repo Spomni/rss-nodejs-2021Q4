b76f04f5380a6d22b2234436972f3a1c65b3e875
spomni <mikhail.snitko@gmail.com>
2021-11-19 12:15:28 +0300

    test: test returning of stdin and stdout from the functions getInputStream() and getOutputStream()

--- ciphering-cli-tool/test/cipher/cipher-helpers.spec.js
+++ ciphering-cli-tool/test/cipher/cipher-helpers.spec.js
@@ -33,19 +33,25 @@ const {
 } = require(fromSrc('cipher/cipher-helpers'))
 
 describe('cipher-helpers', () => {
-  
+
   describe('getInputStream():', function () {
 
-    it('Should return an instance of the InputSteam class', function () {
-      expect(getInputStream()).toBeInstanceOf(InputStream)
+    it('Should retun process.stdin if the input option is not passed', function () {
+      expect(getInputStream()).toBe(process.stdin)
+    })
+
+    it('Should return an instance of the InputSteam class if the input option is passed', function () {
       expect(getInputStream('some/path')).toBeInstanceOf(InputStream)
     })
   })
-  
+
   describe('getOutputStream():', function () {
 
-    it('Should return an instance of the OutputSteam class', function () {
-      expect(getOutputStream()).toBeInstanceOf(OutputStream)
+    it('Should retun process.stdout if the output option is not passed', function () {
+      expect(getOutputStream()).toBe(process.stdout)
+    })
+
+    it('Should return an instance of the OutputSteam classif the output option is passed', function () {
       expect(getOutputStream('some/path')).toBeInstanceOf(OutputStream)
     })
   })
@@ -55,14 +61,14 @@ describe('cipher-helpers', () => {
     it('Should return an array', function () {
       expect(getTransformList('A-C0-R0')).toBeArray()
     })
-    
+
     it('Should place an AtbashTransformer instance at the same position where acommand "A" is', function () {
       const config = 'R1-A-C1-A'
       const transformList = getTransformList(config)
-      
+
       config.split('-').forEach((command, index) => {
         if (command !== 'A') return
-        
+
         expect(transformList[index]).toBeInstanceOf(AtbashTransformer)
       })
     })
@@ -70,10 +76,10 @@ describe('cipher-helpers', () => {
     it('Should place an CaesarEncoder instance at the same position where a command "C1" is', function () {
       const config = 'R1-A-C1-A-R0-C1-C0'
       const transformList = getTransformList(config)
-      
+
       config.split('-').forEach((command, index) => {
         if (command !== 'C1') return
-        
+
         expect(transformList[index]).toBeInstanceOf(CaesarEncoder)
       })
     })
@@ -81,10 +87,10 @@ describe('cipher-helpers', () => {
     it('Should place an CaesarDecoder instance at the same position where a command "C0" is', function () {
       const config = 'C0-A-C1-A-R0-C1-C0'
       const transformList = getTransformList(config)
-      
+
       config.split('-').forEach((command, index) => {
         if (command !== 'C0') return
-        
+
         expect(transformList[index]).toBeInstanceOf(CaesarDecoder)
       })
     })
@@ -92,10 +98,10 @@ describe('cipher-helpers', () => {
     it('Should place an Rot8Encoder instance at the same position where a command "R1" is', function () {
       const config = 'R1-A-C1-R1-R0-C1-C0'
       const transformList = getTransformList(config)
-      
+
       config.split('-').forEach((command, index) => {
         if (command !== 'R1') return
-        
+
         expect(transformList[index]).toBeInstanceOf(Rot8Encoder)
       })
     })
@@ -103,10 +109,10 @@ describe('cipher-helpers', () => {
     it('Should place an Rot8Decoder instance at the same position where a command "R0" is', function () {
       const config = 'R1-A-C1-R0-R0-C1-C0'
       const transformList = getTransformList(config)
-      
+
       config.split('-').forEach((command, index) => {
         if (command !== 'R0') return
-        
+
         expect(transformList[index]).toBeInstanceOf(Rot8Decoder)
       })
     })
