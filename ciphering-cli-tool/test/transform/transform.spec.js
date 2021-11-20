cd2bfd24e69edf071c4eb16036ab84ec8216c59c
spomni <mikhail.snitko@gmail.com>
2021-11-20 22:47:13 +0300

    test(draft): describe transform test cases

---
+++ ciphering-cli-tool/test/transform/transform.spec.js
@@ -0,0 +1,31 @@
+describe('transform', function () {
+
+  describe('exports', function () {
+    it.todo('Should export RotTransformer')
+    it.todo('Should export AtbashTransformer')
+  });
+
+  describe('CaesarEncoder', function () {
+    it.todo('Should extend RotTransformer')
+    it.todo('Should have shifting equal to 1')
+    it.todo('Should have direction RIGHT')
+  });
+
+  describe('CaesarDecoder', function () {
+    it.todo('Should extend RotTransformer')
+    it.todo('Should have shifting equal to 1')
+    it.todo('Should have direction LEFT')
+  });
+
+  describe('Rot8Encoder', function () {
+    it.todo('Should extend RotTransformer')
+    it.todo('Should have shifting equal to 8')
+    it.todo('Should have direction RIGHT')
+  });
+
+  describe('Rot8Encoder', function () {
+    it.todo('Should extend RotTransformer')
+    it.todo('Should have shifting equal to 8')
+    it.todo('Should have direction LEFT')
+  });
+})
