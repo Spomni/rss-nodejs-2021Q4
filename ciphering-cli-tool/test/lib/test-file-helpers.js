5475c0ce75276fa08a40bca5a90e83785cc64dc0
spomni <mikhail.snitko@gmail.com>
2021-11-20 15:27:50 +0300

    test(helpers): add file helpers

---
+++ ciphering-cli-tool/test/lib/test-file-helpers.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const fs = require('fs')
+
+const srcPath = path.resolve(__dirname, '../lib/')
+const testPath = path.resolve(__dirname, '../')
+const fixturePath = path.join(testPath, 'fixture/')
+
+function fromSrc(file) {
+  return path.join(srcPath, file)
+}
+
+function fromTest(file) {
+  return path.join(testPath, file)
+}
+
+function fromFixture(file) {
+  return path.join(fixturePath, file)
+}
+
+function getIOFilesHelpers(testName) {
+
+  const inputPath = path.join(fixturePath, `${testName}-input`)
+  const outputPath = path.join(fixturePath, `${testName}-output`)
+
+  return {
+
+    inputPath,
+    outputPath,
+
+    clearIOFiles: function () {
+      fs.writeFileSync(inputPath, '')
+      fs.writeFileSync(outputPath, '')
+    },
+
+    removeIOFiles: function () {
+      fs.unlinkSync(inputPath)
+      fs.unlinkSync(outputPath)
+    },
+  }
+}
+
+module.exports = {
+
+  srcPath,
+  testPath,
+  fixturePath,
+
+  fromSrc,
+  fromTest,
+  fromFixture,
+
+  getIOFilesHelpers,
+}
