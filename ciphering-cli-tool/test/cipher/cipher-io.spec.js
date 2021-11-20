3d796a8c55f97e14d8e38b169cd479a78b02a0cb
spomni <mikhail.snitko@gmail.com>
2021-11-20 15:32:50 +0300

    test: test InputStream class

--- ciphering-cli-tool/test/cipher/cipher-io.spec.js
+++ ciphering-cli-tool/test/cipher/cipher-io.spec.js
@@ -1,12 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const { Readable, PassThrough } = require('stream')
+
+const { promisifyListener } = require('../lib/promisify-listener')
+const { getIOFilesHelpers  } = require('../lib/test-file-helpers')
+
+const {
+  inputPath,
+  outputPath,
+  clearIOFiles,
+  removeIOFiles,
+} = getIOFilesHelpers('cipher-io')
+
+const { InputStream } = require('../../lib/cipher/cipher-io')
 
 describe('cipher-io', function () {
 
+  beforeEach(clearIOFiles)
+  afterAll(removeIOFiles)
+
   describe('InputStream', function () {
-    it.todo('Should return a Readable stream instance')
-    it.todo('Should read data from the passed file')
-    it.todo('Should be ended when an input file is readed')
+
+    it('Should return a Readable stream instance', function (done) {
+      const inputStream = new InputStream(inputPath)
+      expect(inputStream).toBeInstanceOf(Readable)
+
+      inputStream.on('resume', () => done())
+      inputStream.resume()
+    })
+
+    it('Should read data from the passed file', async function () {
+
+      const content = 'asd\nasdw'
+      fs.writeFileSync(inputPath, content)
+
+      const inputStream = new InputStream(inputPath)
+      const ac = new AbortController()
+
+      await promisifyListener(inputStream, 'data', (data) => {
+        expect(data.toString()).toBe(content)
+        ac.abort()
+      }, ac.signal)
+    })
   })
-  
+
   describe('OutputStream', function () {
     it.todo('Should return a Writable stream instance')
     it.todo('Should append data to the passed file')
