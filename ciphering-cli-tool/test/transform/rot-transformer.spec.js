428947625c2ca78c9b5c201e3059fb7504b26560
spomni <mikhail.snitko@gmail.com>
2021-11-21 00:19:58 +0300

    test: test RotTransformer._transform()

--- ciphering-cli-tool/test/transform/rot-transformer.spec.js
+++ ciphering-cli-tool/test/transform/rot-transformer.spec.js
@@ -1,10 +1,40 @@
-const { Transform } = require('stream')
+const { Transform, Readable, PassThrough } = require('stream')
 
 const matchers = require('jest-extended')
 expect.extend(matchers);
 
 const { RotTransformer } = require('../../lib/transform/rot-transformer')
 const { DIRECTION_ } = RotTransformer
+
+const CODE_ = {
+  LOWER_A: 97,
+  LOWER_Z: 122,
+  UPPER_A: 65,
+  UPPER_Z: 90,
+}
+
+async function testTransformer(transformer, inputData, callback) {
+  return new Promise((resolve, reject) => {
+
+    const input = new Readable({ read: () => {} })
+    const output = new PassThrough()
+
+    output.on(`data`, (data) => {
+      try {
+        callback(data)
+        resolve()
+      } catch (error) {
+        reject(error)
+      }
+    })
+
+    input
+      .pipe(transformer)
+      .pipe(output)
+
+    input.push(inputData)
+  })
+}
 
 describe('RotTransformer', () => {
 
@@ -58,8 +88,65 @@ describe('RotTransformer', () => {
   });
 
   describe('_transform', () => {
-    it.todo('Should increase codes by shifting if direction is RIGHT')
-    it.todo('Should decrease codes by shifting if direction is LEFT')
-    it.todo('Should change only codes of the english alphabet')
+
+    it('Should increase codes by shifting if direction is RIGHT', async function () {
+
+      const shifting = 7
+
+      const fixture = [
+        [CODE_.LOWER_A, CODE_.LOWER_A + shifting],
+        [CODE_.LOWER_Z, CODE_.LOWER_A + shifting - 1],
+        [CODE_.LOWER_A + 13, CODE_.LOWER_A + 13 + shifting],
+        [CODE_.UPPER_A, CODE_.UPPER_A + shifting],
+        [CODE_.UPPER_Z, CODE_.UPPER_A + shifting - 1],
+        [CODE_.UPPER_A + 13, CODE_.UPPER_A + 13 + shifting],
+      ]
+
+      const rot = new RotTransformer(shifting, DIRECTION_.RIGHT)
+
+      for (let [input, output] of fixture) {
+        await testTransformer(rot, Buffer.from([input]), (data) => {
+          expect(data[0]).toBe(output)
+        })
+      }
+    })
+
+    it('Should decrease codes by shifting if direction is LEFT', async function () {
+
+      const shifting = 12
+
+      const fixture = [
+        [CODE_.LOWER_Z, CODE_.LOWER_Z - shifting],
+        [CODE_.LOWER_A, CODE_.LOWER_Z - shifting + 1],
+        [CODE_.LOWER_A + 13, CODE_.LOWER_A + 13 - shifting],
+        [CODE_.UPPER_Z, CODE_.UPPER_Z - shifting],
+        [CODE_.UPPER_A, CODE_.UPPER_Z - shifting + 1],
+        [CODE_.UPPER_A + 13, CODE_.UPPER_A + 13 - shifting],
+      ]
+
+      const rot = new RotTransformer(shifting, DIRECTION_.LEFT)
+
+      for (let [input, output] of fixture) {
+        await testTransformer(rot, Buffer.from([input]), (data) => {
+          expect(data[0]).toBe(output)
+        })
+      }
+    })
+
+    it('Should change only codes of the english alphabet', async function () {
+
+      const input = [
+        CODE_.LOWER_A - 1,
+        CODE_.LOWER_Z + 1,
+        CODE_.UPPER_A - 1,
+        CODE_.UPPER_Z + 1,
+      ]
+
+      const rot = new RotTransformer(26, DIRECTION_.RIGHT)
+
+      await testTransformer(rot, Buffer.from(input), (data) => {
+        expect([...data]).toEqual(input)
+      })
+    })
   });
 });
